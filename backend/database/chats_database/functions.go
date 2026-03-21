4b47c4df75ea846f046fc8a1ce44d8d66b38d556
Wizer27 <svd2yar@gmail.com>
2026-03-21 18:01:49 +0300

    chats table go coded

--- backend/database/chats_database/functions.go
+++ backend/database/chats_database/functions.go
@@ -1,1 +1,78 @@
 package chatsdatabase
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func NewPool(databaseURL string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	config.MaxConns = 20
+	config.MinConns = 5
+	config.MaxConnLifetime = time.Hour
+	config.MaxConnIdleTime = 30 * time.Minute
+	config.HealthCheckPeriod = time.Minute
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func Create_Chat(ctx context.Context, pool *pgxpool.Pool, email string) string {
+	id := uuid.New()
+
+	id_string := id.String()
+
+	_, err := pool.Exec(
+		ctx,
+		`INSERT INTO chats_table (email,chat_id) VALUES($1,$2)`,
+		email, id_string,
+	)
+	if err != nil {
+		return ""
+	}
+	return id_string
+}
+
+func main() {
+	err_env := godotenv.Load()
+	if err_env != nil {
+		log.Fatal(err_env)
+	}
+
+	db_psw := os.Getenv("DB_PASSWORD")
+	db_user := os.Getenv("DB_USER")
+
+	database_url := "postgres://" + db_user + ":" + db_psw + "@localhost:5432/chats_database"
+
+	pool, err := NewPool(database_url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer pool.Close()
+
+	ctx := context.Background()
+
+	res := Create_Chat(ctx, pool, "test@gmail.com")
+
+	fmt.Println(res)
+}
