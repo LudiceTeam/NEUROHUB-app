1eeeb516c05393dd46337f89b2331c693ed8738f
Wizer27 <svd2yar@gmail.com>
2026-03-26 18:26:07 +0300

    api init

---
+++ backend/api/api.py
@@ -0,0 +1,68 @@
+from fastapi import Depends,HTTPException,Request,FastAPI,Header,status,File,UploadFile
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from pydantic import BaseModel
+import uvicorn
+import json
+import hmac
+import hashlib
+import asyncio
+import os
+from dotenv import load_dotenv
+import time
+from slowapi import Limiter,_rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import timedelta
+from typing import Optional
+
+load_dotenv()
+
+app = FastAPI()
+
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(429, _rate_limit_exceeded_handler)
+#app.add_middleware(HTTPSRedirectMiddleware)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+async def verify_signature(data: dict, rec_signature, x_timestamp: str) -> bool:
+   
+    if time.time() - int(x_timestamp) > 300:
+        return False
+    
+   
+    return await asyncio.to_thread(_sync_verify_signature, data, rec_signature)
+
+def _sync_verify_signature(data: dict, rec_signature: str) -> bool:
+   
+    KEY = os.getenv("signature")
+    data_to_verify = data.copy()
+    data_to_verify.pop("signature", None)
+    data_str = json.dumps(data_to_verify, sort_keys=True, separators=(',', ':'))
+    expected = hmac.new(KEY.encode(), data_str.encode(), hashlib.sha256).hexdigest()
+    return hmac.compare_digest(rec_signature, expected)
+
+
+
+
+async def safe_get(req: Request):
+    try:
+        api = req.headers.get("X-API-KEY")
+        if not api:
+            raise HTTPException(status_code=401, detail="Invalid API key")
+        
+        if not await asyncio.to_thread(hmac.compare_digest, api, os.getenv("api")):
+            raise HTTPException(status_code=401, detail="Invalid API key")
+            
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid api key")
+
+
+# --- ROUTES ---
+
+async def main():
+    return "NEUROHUB-API"
+
