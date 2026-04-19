e0fe1d55ce7d06622b9cdbe36fef99570883246f
Wizer27 <svd2yar@gmail.com>
2026-04-19 20:35:47 +0300

    some fixes

--- backend/api/deployer.py
+++ backend/api/deployer.py
@@ -6,7 +6,7 @@ from backend.database.ai_choose_db.ai_core import create_table as cr5
 from backend.database.apple_notification_log.apple_core import create_table as cr6
 from backend.database.transaction_db.transaction_core import create_table as cr7
 from backend.database.stats_db.stats_core import create_table as cr9,write_default
-
+from backend.database.email_code_db.email_core import create_table as cr8
 
 from backend.database.main_database.main_core import drop_table as dr1
 from  backend.database.chats_database.chats_core import drop_table as dr2
@@ -23,7 +23,7 @@ import asyncio
 
 
 
-function_arr = [cr1,cr2,cr3,cr4,cr5,cr6,cr7,cr9]
+function_arr = [cr1,cr2,cr3,cr4,cr5,cr6,cr7,cr8,cr9]
 
 function_arr_drop = [dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr9]
 
@@ -37,7 +37,6 @@ async def create_all():
         await func()
 
 if __name__ == "__main__":
-    pass
     #asyncio.run(write_default())
     #asyncio.run(drop_all())
-    #asyncio.run(create_all())
+    asyncio.run(create_all())
