23a688198e09f80dd994aae3a454fafda8205917
Wizer27 <svd2yar@gmail.com>
2026-04-26 00:26:20 +0300

    devices table init

---
+++ backend/database/devices_db/devices_core.py
@@ -0,0 +1,44 @@
+from backend.database.devices_db.devices_models import metadata_obj,devices_table
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+import asyncio
+import logging
+import uuid
+from typing import List,Optional
+from sqlalchemy import select,func
+from datetime import datetime,timezone
+from backend.api.psw_hash import decrypt,encrypt
+from backend.api.config import database_url 
+
+logger = logging.getLogger(__name__)
+
+
+load_dotenv()
+
+async_engine = create_async_engine(
+    database_url,
+    pool_size=20,          
+    max_overflow=50,       
+    pool_recycle=3600,    
+    pool_pre_ping=True,     
+    echo=False,
+    connect_args={"ssl": "require"},
+)
+
+
+
+AsyncSessionLocal = sessionmaker(
+    async_engine, 
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def drop_table():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(metadata_obj.drop_all)
+
+async def create_table():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(metadata_obj.create_all)
---
+++ backend/database/devices_db/devices_models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Table,Column,MetaData,String,DateTime
+
+metadata_obj = MetaData()
+
+devices_table = Table(
+    "devices_table",
+    metadata_obj,
+    Column("user_id",String),
+    Column("device_id",String,unique=True,primary_key=True),
+    Column("last_online",DateTime(timezone=True))
+)
