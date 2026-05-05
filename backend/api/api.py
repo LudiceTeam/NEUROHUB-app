26c9420c149a6a8c91417cf9821b90c7abda4840
Wizer27 <svd2yar@gmail.com>
2026-05-05 22:19:02 +0300

    .

--- backend/api/api.py
+++ backend/api/api.py
@@ -1715,6 +1715,7 @@ async def change_avatar_handler(request:Request,avatar:UploadFile = File(...),us
                 )
 
         if avatar.content_type not in ["image/jpeg", "image/png", "image/webp","image/jpg"]:
+                print("file type error")
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
                     detail="Unsupported file type"
