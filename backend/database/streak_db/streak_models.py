cf871af8576661eafc12386e5b4274424ef0c8a2
Wizer27 <svd2yar@gmail.com>
2026-06-03 19:21:07 +0300

    streak db coded

--- backend/database/streak_db/streak_models.py
+++ backend/database/streak_db/streak_models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Table,Column,MetaData,String,Integer
+from sqlalchemy import Table,Column,MetaData,String,Integer,Date
+from datetime import datetime
+
 
 metadata_obj = MetaData()
 
@@ -6,5 +8,6 @@ streak_table = Table(
     "streak_table",
     metadata_obj,
     Column("user_id",String,primary_key=True,unique=True),
-    Column("streak",Integer) # days of streak
+    Column("streak",Integer),# days of streak
+    Column("last_updated",Date, default=datetime.now().date)
 )
