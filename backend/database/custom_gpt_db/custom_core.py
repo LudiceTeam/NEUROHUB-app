d82cc2a6b71964cc54acf6cdc83d6bddb44004df
Wizer27 <svd2yar@gmail.com>
2026-08-13 14:46:59 +0300

    working on the new fitch

---
+++ backend/database/custom_gpt_db/custom_core.py
@@ -0,0 +1,27 @@
+from backend.database.custom_gpt_db.custom_models import metadata_obj,custom_table
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+import asyncio
+import logging
+import uuid
+from typing import List,Optional,Dict
+from sqlalchemy import select,func
+from datetime import datetime,timezone,timedelta
+from backend.api.psw_hash import decrypt,encrypt
+from backend.api.config import async_engine
+
+
+logger = logging.getLogger(__name__)
+
+
+
+
+async def drop_table():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(metadata_obj.drop_all)
+
+async def create_table():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(metadata_obj.create_all)
---
+++ backend/database/custom_gpt_db/custom_models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import MetaData,Table,Column,String
+
+metadata_obj = MetaData()
+
+custom_table = Table(
+    "custom_table",
+    metadata_obj,
+    Column("user_id",String),
+    Column("name",String),
+    Column("gpt_id",String,primary_key=True,unique=True),
+    Column("gpt_promt",String)
+)
