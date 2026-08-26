65f9f0ce0401a3e14c0e6543cac7eb5c6bfd3559
Wizer27 <svd2yar@gmail.com>
2026-08-26 21:30:39 +0300

    ?

--- backend/database/custom_gpt_select_db/select_core.py
+++ backend/database/custom_gpt_select_db/select_core.py
@@ -56,3 +56,4 @@ async def get_user_gpt(user_id:str) -> str | None:
         except Exception:
             logger.exception("SELECT CUSTOM GPT ERROR")
             return None
+#write some tests here
